fde6fbef78a716a8108c9b2d3d01abccca7beb51
alexpich <pich.alex01@gmail.com>
2019-01-21 11:01:29 -0500

    Added createItem mutation and fixed typo in db.js

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,20 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    console.log(item);
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
