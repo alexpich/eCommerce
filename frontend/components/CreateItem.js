ecea0aa90a82bac0955958f7f8c7a5e70477bb4f
alexpich <pich.alex01@gmail.com>
2019-01-21 17:09:04 -0500

    Added uploadFile function to handle image uploads

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -41,6 +41,31 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    console.log("uploading file");
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    // For cloudinary
+    data.append("upload_preset", "eCommerce");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/bpeach/image/upload",
+      {
+        method: "POST",
+        body: data
+      }
+    );
+
+    const file = await res.json();
+    console.log(file);
+
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -58,6 +83,21 @@ class CreateItem extends Component {
           >
             <ErrorMessage error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload Image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img width="200" src={this.state.image} alt="Image Preview" />
+                )}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input
