30643f5c703fe57ceeeba1122dc5574e3be1c059
alexpich <pich.alex01@gmail.com>
2019-01-20 20:12:56 -0500

    Created the Prisma db and GraphQL Yoga Server

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+// File connects to remote prisma DB and gives ability to query it with JS
+const { Prisma } = require("prisma-binding");
+
+const db = new Prisma({
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+modules.export  = db;
