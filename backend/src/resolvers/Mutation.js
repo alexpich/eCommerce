704dc01857d3a43b56828e637a7c4435d4e9c1c0
alexpich <pich.alex01@gmail.com>
2019-01-21 21:09:50 -0500

    Made more query and mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -14,6 +14,24 @@ const Mutations = {
     console.log(item);
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // Take a copy of the updates
+    const updates = { ...args };
+
+    // Remove the ID from the updates
+    delete updates.id;
+
+    // Run update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
