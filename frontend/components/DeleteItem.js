d6a53239d17784ac21cb01b59859cc66019f3856
alexpich <pich.alex01@gmail.com>
2019-01-23 19:18:27 -0500

    Added deleteItem function to delete items

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import { ALL_ITEMS_QUERY } from "./Items";
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
 
 class DeleteItem extends Component {
+  update = (cache, payload) => {
+    // manually update the cache on the client so it matches the server
+    // 1. Read the cache
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    console.log(data, payload);
+
+    // 2. Filter the deleted item out of the page
+    data.items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id
+    );
+    // 3. Put items back
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
   render() {
-    return <button>Delete</button>;
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{
+          id: this.props.id
+        }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button
+            onClick={() => {
+              if (confirm("Confirm deletion?")) {
+                deleteItem();
+              }
+            }}
+          >
+            {this.props.children}
+          </button>
+        )}
+      </Mutation>
+    );
   }
 }
 
