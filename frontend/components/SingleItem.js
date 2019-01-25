fa563da678fe3f9287cbf4048a0dd833f6ac59af
alexpich <pich.alex01@gmail.com>
2019-01-24 23:47:17 -0500

    Added more details

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -13,6 +13,15 @@ const SingleItemStyles = styled.div`
   grid-auto-columns: 1fr;
   grid-auto-flow: column;
   min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
 `;
 
 const SINGLE_ITEM_QUERY = gql`
@@ -41,6 +50,10 @@ class SingleItem extends Component {
                 <title> eCommerce | {data.item.title}</title>
               </Head>
               <img src={data.item.largeImage} alt="data.item.title" />
+              <div className="details">
+                <h2>Viewing {data.item.title}</h2>
+                <p>{data.item.description}</p>
+              </div>
             </SingleItemStyles>
           );
         }}
