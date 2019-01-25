0133d435dc4db9b578297f40154bd5d54899b52b
alexpich <pich.alex01@gmail.com>
2019-01-25 00:42:09 -0500

    Created prev and next buttons for pagination

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,10 +1,59 @@
+import gql from "graphql-tag";
+import Head from "next/head";
 import React from "react";
+import { Query } from "react-apollo";
+import { perPage } from "../config";
 import PaginationStyles from "./styles/PaginationStyles";
+import Link from "next/link";
+
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    itemsConnection {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
 
 const Pagination = props => (
-  <PaginationStyles>
-    <p>THis is pagiantion component</p>
-  </PaginationStyles>
+  <Query query={PAGINATION_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <p>Loading...</p>;
+      const count = data.itemsConnection.aggregate.count;
+      const page = props.page;
+      const pages = Math.ceil(count / perPage);
+      return (
+        <PaginationStyles>
+          <Head>
+            <title>
+              eCommerce | Page {page} of {pages}{" "}
+            </title>
+          </Head>
+          <Link
+            prefetch
+            href={{ pathname: "items", query: { page: page - 1 } }}
+          >
+            <a className="prev" aria-disabled={page <= 1}>
+              Prev
+            </a>
+          </Link>
+          <p>
+            Page {page} of {pages}
+          </p>
+          <p>{count} total</p>
+          <Link
+            prefetch
+            href={{ pathname: "items", query: { page: page + 1 } }}
+          >
+            <a className="next" aria-disabled={page >= pages}>
+              Next
+            </a>
+          </Link>
+        </PaginationStyles>
+      );
+    }}
+  </Query>
 );
 
 export default Pagination;
