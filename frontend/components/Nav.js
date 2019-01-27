787c94183cbc294b3df0863e579b2481881d55b6
alexpich <pich.alex01@gmail.com>
2019-01-26 23:00:17 -0500

    Handle current logged in user

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,8 +1,16 @@
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
+import User from "./User";
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({ data: { me } }) => {
+        console.log(me);
+        if (me) return <p>{me.name}</p>;
+        return null;
+      }}
+    </User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
@@ -16,7 +24,7 @@ const Nav = () => (
       <a>Orders</a>
     </Link>
     <Link href="/account">
-      <a>Account</a> 
+      <a>Account</a>
     </Link>
   </NavStyles>
 );
