2cde0fdc91fe68c5e58d58aebbf8ef63fb5737fc
alexpich <pich.alex01@gmail.com>
2019-01-27 22:05:47 -0500

    Set up the schema and mutation for logging out

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -99,6 +99,10 @@ const Mutations = {
 
     // Return user
     return user;
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie("token");
+    return { message: "Sign out successful!" };
   }
 };
 
